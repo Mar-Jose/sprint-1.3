606d410ad77e9a4a98a7536fbc0570f8ca1e0966
Mar José <de.la.taes@gmail.com>
2025-10-03 13:42:37 -0300

    corrección de errores 3

--- index.mjs
+++ index.mjs
@@ -3,10 +3,10 @@ import {leerSuperheroes, agregarSuperheroes} from './utils.mjs';
 const archivoOriginal = './superheroes.txt';
 const archivoNuevos = './agregarSuperheroes.txt';
 
-//Agregar nuevos superheroes
+//Agregar nuevos superheroes:
 agregarSuperheroes(archivoOriginal, archivoNuevos);
 
-//leer y mostrar la lista de superheroes ordenada
-const superheroes = leerSuperheroes('./superheroes.txt');
+//leer y mostrar la lista de superheroes ordenada:
+const superheroes = leerSuperheroes('archivoOriginal');
 console.log('Superheroes ordenados:');
 console.log(superheroes);
